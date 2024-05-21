28b1d726a3a859479c0c3bd3431ad3340b447adb
Михаил Курочкин <eaq19933@gmail.com>
2024-05-21 21:59:23 +0300

    Added some fun on master branch

--- src/main.kt
+++ src/main.kt
@@ -1,8 +1,14 @@
 fun main() {
     val a = 4
     println("The sqr root of $a is ${sqrPower(a)}")
+    sayHiFromMaster()
 }
 
 fun sqrPower(x: Int): Int {
     return x * x
+}
+
+//added some function parallel in master branch, while some changes in other branches
+fun sayHiFromMaster() {
+    println("HI! I'm a master!")
 }
